bd93f6919f36184f0f8e99ac8a6f4fa6316fcba8
MR.M.T.AUSTWICK <U1856203@hud.ac.uk>
2018-11-18 08:29:10 +0000

    Created the emptywaste command

--- emptywaste.bash
+++ emptywaste.bash
@@ -1,2 +1,9 @@
 #!/bin/bash
 # emptywaste.bash - A command which permanently deletes everything in the waste bin
+
+echo "The program will now delete everything inside of your secret .waste directory"
+echo "...Deleting files now..."
+rm -r ~/.waste/*
+#The line above deletes everything inside of the .waste directory including
+#any sub directories, forever.
+echo "...All files have now been deleted..."
