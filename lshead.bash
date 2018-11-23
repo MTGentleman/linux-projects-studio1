3c9fc91c429184c2c6786e146bccedac9b7e235b
MR.M.T.AUSTWICK <U1856203@hud.ac.uk>
2018-11-23 12:50:51 +0000

    Changed to output something if neither or the tests are true

--- lshead.bash
+++ lshead.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# lshead.bash - A program which reads the first X lines or the last X lines of all files in a given directory
+#author - Matthew Austwick, 2018
 
 if [ $# -lt 3 ]
 then
@@ -7,18 +9,23 @@ then
 fi
 dir_content=$(ls $3)
 for file in $dir_content
+#This line repeats the below section of code as many times as there are files in the directory
 do
-	if [ -d $file ]
+	if [ -d $file ] #If the selected file is a directory, it cannot be worked with in this program, so it skips it.
 	then
 		echo "This file is a directory, the program will not process this directory, but will continue"
 		continue
 	fi
 	if [ $1 == "head" ]
+	#If the user inputted "head" into one of their arguments
 	then
 		file_content=$(head -$2 $3/$file)
 	elif [ $1 == "tail" ]
 	then
+	#If the user inputted "tail" into one of their arguments
 		file_content=$(tail -$2 $3/$file)
+	else
+		echo "Your arguments are invalid, please try again"
 	fi
 	echo Your file $file contains $file_content
 	echo " "
