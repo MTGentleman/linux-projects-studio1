374d91ad356f0c645bd151fdf127bf9dd24cdeda
MR.M.T.AUSTWICK <U1856203@hud.ac.uk>
2018-12-11 10:09:39 +0000

    changed it a bit

--- textmasher.bash
+++ textmasher.bash
@@ -1,26 +1,31 @@
 #! /bin/bash
 # textmasher.bash - A program which initially takes 1 argument then repeats endlessly until the user enters a * somewhere within their string input
 # author - Matthew Austwick, designed for Task 3 of the 1st Assignment of Studio1
-
-if [ $# -ne 1 ] #This if statement merely checks to see if the user hasn't entered anything
+test=1
+if [ $# -lt 1 ] #This if statement merely checks to see if the user hasn't entered anything
 then
-	echo "Sorry, but this command can only have 1 initial argument. Please run the command again with 1 argument"
+	echo "Sorry, but this command can only have 1 or more  initial arguments. Please run the command again with 1 or more arguments"
 	exit
 fi
+text="$1" # This first saves the user's input into a variable which is used to compare and store other entered text
+if [[ "$text" == *"*"* ]]
+then
+	test=2
+fi
+#This if statement checks as to whether or not their initial string as a * in it; if it does, it changes the value of test so the while loop doesn't run and it just skips to where it counts up the total number of each type of character in the string
 echo "Welcome to the text masher!"
 echo "You first value to check is $1"
-echo "Please enter characters (digits, numbers or otherwise) and this program will count how many of each there are!"
-echo "If you enter a '*' anywhere within the string, the program will terminate and display the aforementioned output"
-#The line above assigns a file to a variable; this saves time because this file will need to be written to and read from multiple times; this saves me from writing out the file name multiple times and over and over again.
-text=$1 #This first saves the user's input into a variable  which is used to store the values of whatever the user enters
-while true; 
+echo "If any (digits, numbers or otherwise) have been entered, this program will count how many of each there are!"
+echo "If you enter (or have entered) a '*' anywhere within the string, the program will terminate and display the aforementioned output"
+
+while [ $test -eq 1 ] 
 do
 	echo "Please enter more characters to be compared"
 	read input
-	curnum=$(echo $input | grep -o "[[:digit:]]" | wc -l)
-	currentalpha=$(echo $input | grep -o "[[:alpha:]]" | wc -l)
-	curpunct=$(echo $input | grep -o "[[:punct:]]" | wc -l)
-	if [[ $input == *"*"* ]]; then
+	curnum=$(echo "$input" | grep -o "[[:digit:]]" | wc -l)
+	currentalpha=$(echo "$input" | grep -o "[[:alpha:]]" | wc -l)
+	curpunct=$(echo "$input" | grep -o "[[:punct:]]" | wc -l)
+	if [[ "$input" == *"*"* ]]; then
 		text="$text$input" #This, similar to the line below, appends to the textmash_results.txt; however, for this line it also includes the terminating * character and what they entered before it. 
 		echo "You had $curnum digits in your last entered string"
 		echo "You have $currentalpha letters in your last entered string"
